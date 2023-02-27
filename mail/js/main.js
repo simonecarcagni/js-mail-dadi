3cffe2458bda7204a4ad59a5f2590b14e662cdcb
Simone Carcagnì <simonecarcagni@libero.it>
2023-02-27 18:03:45 +0100

    Lavoro Terminato

--- mail/js/main.js
+++ mail/js/main.js
@@ -6,12 +6,45 @@ let mailRequest = prompt('Inserisci la tua email');
 
 console.log(mailRequest);
 
+let validity = false;
+
 for (let i = 0; i < mailList.length; i++) {
 
-    if (mailRequest == mailList[i]){
-        console.log(`La tua mail ${mailRequest} è valida: puoi accedere.`);
+    if (mailRequest == mailList[i]) {
+        validity = true;
     };
 
 };
 
+if (validity){
+    console.log(`La tua mail ${mailRequest} è valida: puoi accedere.`);
+} else {
+    console.log(`La tua mail ${mailRequest} non è valida: non puoi accedere.`);
+};
 
+
+
+/*
+for (let i = 0; i < mailList.length; i++) {
+
+    if (mailRequest == mailList[i]){
+        console.log(`La tua mail ${mailRequest} è valida: puoi accedere.`);
+    } else if (mailRequest !== mailList[i]){
+        console.log(`NO`)
+    };
+
+};
+*/
+
+const valori = [1, 2, 5, 7];
+
+let somma = 0;
+
+for (let i = 0; i < valori.length; i++){
+
+    somma += valori[i];
+    result = somma / (i + 1);
+};
+
+console.log(somma);
+console.log(result);
